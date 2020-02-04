5c8f22ef2a8e7b30e6569544955b356592edbd88
amird <amir.dantas@hotmail.com>
2020-02-04 15:02:17 -0300

    model create, eslint, sequelize

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-import express from 'express'
-import routes from './routes'
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
@@ -13,7 +13,7 @@ class App {
     this.server.use(express.json());
   }
 
-  routes (){
+  routes() {
     this.server.use(routes);
   }
 }
